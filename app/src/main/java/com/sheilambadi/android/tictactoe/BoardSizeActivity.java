b979c69bfce525088f59e7b9458c29def4c40d93
sheilambadi <mbadiatieno@gmail.com>
2018-04-10 12:21:43 +0300

    add board size

--- app/src/main/java/com/sheilambadi/android/tictactoe/BoardSizeActivity.java
+++ app/src/main/java/com/sheilambadi/android/tictactoe/BoardSizeActivity.java
@@ -1,15 +1,13 @@
 package com.sheilambadi.android.tictactoe;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class BoardSizeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +19,20 @@ public class MainActivity extends AppCompatActivity {
                 .setFontAttrId(R.attr.fontPath)
                 .build()
         );
-        setContentView(R.layout.activity_main);
-        //getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        //getSupportActionBar().setCustomView(R.layout.ab_layout);
-    }
 
-    public void singlePlayer(View view){
-        Intent i = new Intent(this, BoardSizeActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_board_size);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_32dp);
     }
 
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
